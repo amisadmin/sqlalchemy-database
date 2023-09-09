7a870399e9d3212a162fdaa4d16323d3a7e00ab8
atomi <1456417373@qq.com>
2023-09-09 18:25:12 +0800

    v0.1.1

--- sqlalchemy_database/__init__.py
+++ sqlalchemy_database/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __url__ = "https://github.com/amisadmin/sqlalchemy_database"
 
 from sqlalchemy_database.database import AbcAsyncDatabase, AsyncDatabase, Database
